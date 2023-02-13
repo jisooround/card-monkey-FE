d23e24692a8f0ecf03db474479258070b7947f9b
jisooround <9utesleben@gmail.com>
2023-02-13 21:12:29 +0900

    Design: 전역 스타일 브라운 컬러 추가 & input 자동완성 배경 컬러 삭제

--- src/global/globalStyles.ts
+++ src/global/globalStyles.ts
@@ -10,11 +10,14 @@ ${reset}
     --color-white: #FFFFFF;
     --color-gray: #BCBCBC;
     --color-lightgray: #EEEEEE;
+    --color-brown: #6B4D29;
     --width-inner : 425px;
     ::selection {
       background-color: var(--color-primary);
       color: var(--color-white);
     }
+    input:-webkit-autofill { -webkit-box-shadow: 0 0 0 30px #fff inset ; -webkit-text-fill-color: #000; }
+    input:-webkit-autofill, input:-webkit-autofill:hover, input:-webkit-autofill:focus, input:-webkit-autofill:active { transition: background-color 5000s ease-in-out 0s; }
 }
 
   body {
