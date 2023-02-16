fb2c977efb314ccd363c89002d16e5304463ed2f
jisooround <9utesleben@gmail.com>
2023-02-16 18:36:59 +0900

    Feat: 회원가입 슬라이스 약관 동의 리듀서 삭제, 혜택 선택 리듀서 추가

--- src/store/signUpSlice.ts
+++ src/store/signUpSlice.ts
@@ -2,43 +2,44 @@ import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface FormState {
-  agree: boolean;
   name: string;
-  id: string;
+  userId: string;
   password: string;
+  benefit: object;
 }
 
 const initialState: FormState = {
-  agree: false,
   name: "",
-  id: "",
+  userId: "",
   password: "",
+  benefit: [],
 };
 
 export const signUpSlice = createSlice({
   name: "form",
   initialState,
   reducers: {
-    fillAgreement(state) {
-      state.agree = true;
-    },
     fillName(state, action) {
       let content = action.payload;
       state.name = content;
     },
     fillId(state, action) {
       let content = action.payload;
-      state.id = content;
+      state.userId = content;
     },
     fillPassword(state, action) {
       let content = action.payload;
       state.password = content;
     },
+    selectBenefit(state, action) {
+      let content = action.payload;
+      state.benefit = content;
+    },
   },
 });
 
 // Action creators are generated for each case reducer function
-export const { fillAgreement, fillName, fillId, fillPassword } =
+export const { fillName, fillId, fillPassword, selectBenefit } =
   signUpSlice.actions;
 
 export default signUpSlice.reducer;
