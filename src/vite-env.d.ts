b1f8a94cb50602efccd00228a2d7bc33e2ef892f
jisooround <9utesleben@gmail.com>
2023-02-22 14:24:39 +0900

    Feat: 아이디 중복 체크 버튼 추가

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -38,14 +38,14 @@ interface BenefitProps {
   item: string;
 }
 
-interface signType {
+interface SignType {
   userId: string;
   password: string;
   name: string;
   benefit: Array<string>;
 }
 
-interface loginType {
+interface LoginType {
   userId: string;
   password: string;
 }
@@ -53,4 +53,8 @@ interface loginType {
 interface Button {
   color: string;
   background: string;
+}
+
+interface IdCheck {
+  userId: string;
 }
