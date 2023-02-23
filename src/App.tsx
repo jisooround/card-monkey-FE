47ae1398b6c22d8048ff627b7d2bfee0a3fce7df
hayden365 <ysmh100@gmail.com>
2023-02-23 19:36:00 +0900

    Feat: toTop버튼 수정

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,9 @@ import {
 
 import Header from "./components/Layout/Header";
 import Navbar from "./components/Layout/Navbar";
+import { SlArrowUp } from "react-icons/sl";
 import throttle from "lodash.throttle";
+import styled from "styled-components";
 
 const App = () => {
   const [scroll, setScroll] = useState(false);
@@ -30,7 +32,6 @@ const App = () => {
   const handleScroll = () => {
     if (window.scrollY >= 300) {
       setScroll(true);
-      console.log(scroll);
     } else {
       setScroll(false);
     }
@@ -39,14 +40,50 @@ const App = () => {
   return token ? (
     <>
       <Header />
-      <main>
+      <Main>
         <Outlet />
-      </main>
-      <Navbar scroll={scroll} />
+        {scroll ? (
+          <BtnTop
+            onClick={() => {
+              window.scrollTo(0, 0);
+            }}
+          >
+            <div className="background"></div>
+            <SlArrowUp size={30} className={"svg"} />
+          </BtnTop>
+        ) : (
+          ""
+        )}{" "}
+      </Main>
+      <Navbar />
     </>
   ) : (
     <Navigate to="/login" />
   );
 };
+
+const Main = styled.main`
+  position: relative;
+`;
+
+const BtnTop = styled.div`
+  position: fixed;
+  right: 150px;
+  bottom: 60px;
+  height: 50px;
+  width: 50px;
+  transition: 0.5s;
+  border: 3px solid var(--color-primary);
+  border-radius: 50%;
+  color: var(--color-primary);
+  background-color: var(--color-white);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  opacity: 0.8;
+  &:hover {
+    opacity: 1;
+  }
+`;
 
 export default App;
